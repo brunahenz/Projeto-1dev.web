6b84e9d8df4af44e4b2e90ada0a2440558c670b4
Bruna Henz <brunaehenz@gmail.com>
2023-03-31 16:48:20 -0300

    mostrar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref } from 'vue'
+
+const nome = ref('')
+const idade = ref(0)
+const email = ref('')
+const ok = true
+
 </script>
 
 <template>
-<h1>Projeto 1</h1>
+  
+  <button>Informe seu nome:</button>
+  <input v-model="nome" />
+  <button @click="nome">Enviar</button>
+  <hr>
+  <button>Informe sua idade:</button>
+  <input v-model="idade" />
+  <button @click="idade">Enviar</button>
+  <hr>
+  <button>Informe seu email:</button>
+  <input v-model="Email" />
+  <button @click="Email">Enviar</button>
+  <div v-if="ok">Mostrar</div>
+  
 </template>
-
-<style scoped>
-</style>
