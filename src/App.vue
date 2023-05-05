7715f467fce817f8adadf76c0c2952bef4a28bf1
brunahenz <brunaehenz@gmail.com>
2023-05-05 16:42:16 -0300

    quase pronto

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const email = ref('')
 const endereco = ref('')
 const cidade = ref('')
 const estado = ref('')
-const hobbie = ref('')
+const hobbie = ref([])
 const linguagem = ref('')
 const biografia = ref('')
 const senha = ref('')
@@ -25,121 +25,128 @@ function handleFileUpload(e) {
     user.value.avatar = URL.createObjectURL(file)
   }
 }
-
+function validar() {
+  if (senha.value === confirma.value) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 </script>
 
 <template>
-<form class="form" >
+  <form class="form" @submit.prevent="ok=validar()">
     <div class="centro ">
-    <label for="">Nome:</label>
-    <input
-    type="file"
-    id="avatarField"
-    @change="handleFileUpload($event)"
-    />
-    <img :src="user.avatar" />
-    <input type="text" v-on:keypress="ok=false" v-model="nome" required placeholder="Digite seu nome" />
-    <hr>
-    <label for="" >Data de nascimento:</label>
-    <input type="date" v-on:keypress="ok=false" v-model="data" placeholder="Digite sua data" />
-    <hr>
-    <label for="" >Email:</label>
-    <input type="email" v-on:keypress="ok=false" v-model="email" placeholder="Digite seu email" />
-    <hr>
-    <label for="" >Endereço:</label>
-    <input type="text" v-on:keypress="ok=false" v-model="endereco" placeholder="Digite seu endereço" />
-    <hr>
-    <label for="" >Cidade:</label>
-    <input type="text" v-on:keypress="ok=false" v-model="cidade" placeholder="Digite sua cidade" />
-    <hr>
-    <label for="estado" >Estado:</label>
-    <select v-on:keypress="ok=false" v-model="estado">
-      <option value="AC">Acre</option>
-      <option value="AL">Alagoas</option>
-      <option value="AP">Amapá</option>
-      <option value="AM">Amazonas</option>
-      <option value="BA">Bahia</option>
-      <option value="CE">Ceará</option>
-      <option value="DF">Distrito Federal</option>
-      <option value="ES">Espírito Santo</option>
-      <option value="GO">Goías</option>
-      <option value="MA">Maranhão</option>
-      <option value="MT">Mato Grosso</option>
-      <option value="MS">Mato Grosso do Sul</option>
-      <option value="MG">Minas Gerais</option>
-      <option value="PA">Pará</option>
-      <option value="PB">Paraíba</option>
-      <option value="PE">Pernambuco</option>
-      <option value="PI">Piauí</option>
-      <option value="RJ">Rio de Janeiro</option>
-      <option value="RN">Rio Grande do Norte</option>
-      <option value="RS">Rio Grande do Sul</option>
-      <option value="RO">Rondônia</option>
-      <option value="RR">Roráima</option>
-      <option value="SC">Santa Catarina</option>
-      <option value="SP">São Paulo</option>
-      <option value="SE">Sergipe</option>
-      <option value="TO">Tocantins</option>
-     </select>
-    <hr>
-    <label for="" >Hobbie:</label>
-    <input type="text" v-on:keypress="ok=false" v-model="hobbie" placeholder="Digite seu hobbie" />
-    <hr>
-    <label for="" >Linguagem de programação:</label>
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="JavaScript" /> JavaScript
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="Python" /> Python
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="C#" /> C#
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="Php" /> Php
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="SQL" /> SQL
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="R" /> R
-    <input type="radio" v-on:keypress="ok=false" v-model="linguagem" value="VBA" /> VBA
-    <hr>
-    <label for="" >Biografia:</label>
-    <input type="text" v-on:keypress="ok=false" v-model="biografia" style="padding: 30px;" placeholder="Digite sua biografia"/>
-    <hr>
-    <label for="" >Senha:</label>
-    <input type="password" v-on:keypress="ok=false" v-model="senha" minlength="6" placeholder="Digite sua senha" />
-    <hr>
-    <label for="" >Confirmar senha:</label>
-    <input type="password" v-on:keypress="ok=false" v-model="confirma" minlength="6" placeholder="Digite sua senha novamente" />
-    <hr>
-    <button @click="ok = !ok">Mostrar</button>
-    </div>
-    <div v-if="ok" class="centro">
-      <p>O nome digitado é: {{ nome }}</p>
-      <p>A data digitada é: {{ data }}</p>
-      <p>O email digitado é: {{ email }}</p>
-      <p>O endereço digitado é: {{ endereco }}</p>
-      <p>A cidade digitada é: {{ cidade }}</p>
-      <p>O estado digitado é: {{ estado }}</p>
-      <p>O hobbie digitado é: {{ hobbie }}</p>
-      <p>A linguagem digitada é: {{ linguagem }}</p>
-      <p>A biografia digitada é: {{ biografia }}</p>
-      <p>A senha digitada é: {{ senha }}</p>
-      <p>A confirmação digitada é: {{ confirma }}</p> 
+      <input type="file" id="avatarField" @change="handleFileUpload($event)" />
+      <label for="">Nome:</label>
+      <input type="text" v-on:keypress="ok = false" v-model="nome" required placeholder="Digite seu nome" />
+      <hr>
+      <label for="">Data de nascimento:</label>
+      <input type="date" v-on:keypress="ok = false" v-model="data" placeholder="Digite sua data" />
+      <hr>
+      <label for="">Email:</label>
+      <input type="email" v-on:keypress="ok = false" v-model="email" placeholder="Digite seu email" />
+      <hr>
+      <label for="">Endereço:</label>
+      <input type="text" v-on:keypress="ok = false" v-model="endereco" placeholder="Digite seu endereço" />
+      <hr>
+      <label for="">Cidade:</label>
+      <input type="text" v-on:keypress="ok = false" v-model="cidade" placeholder="Digite sua cidade" />
+      <hr>
+      <label for="estado">Estado:</label>
+      <select v-on:keypress="ok = false" v-model="estado">
+        <option value="AC">Acre</option>
+        <option value="AL">Alagoas</option>
+        <option value="AP">Amapá</option>
+        <option value="AM">Amazonas</option>
+        <option value="BA">Bahia</option>
+        <option value="CE">Ceará</option>
+        <option value="DF">Distrito Federal</option>
+        <option value="ES">Espírito Santo</option>
+        <option value="GO">Goías</option>
+        <option value="MA">Maranhão</option>
+        <option value="MT">Mato Grosso</option>
+        <option value="MS">Mato Grosso do Sul</option>
+        <option value="MG">Minas Gerais</option>
+        <option value="PA">Pará</option>
+        <option value="PB">Paraíba</option>
+        <option value="PE">Pernambuco</option>
+        <option value="PI">Piauí</option>
+        <option value="RJ">Rio de Janeiro</option>
+        <option value="RN">Rio Grande do Norte</option>
+        <option value="RS">Rio Grande do Sul</option>
+        <option value="RO">Rondônia</option>
+        <option value="RR">Roráima</option>
+        <option value="SC">Santa Catarina</option>
+        <option value="SP">São Paulo</option>
+        <option value="SE">Sergipe</option>
+        <option value="TO">Tocantins</option>
+      </select>
+      <hr>
+      <label for="hobbie">Hobbie:</label>
+      <input class="opcao" type="checkbox" v-model="hobbie" v-on:keypress="ok = false" value="Esporte" /> Esporte
+      <input class="opcao" type="checkbox" v-model="hobbie" v-on:keypress="ok = false" value="Video Game" /> Video Game
+      <input class="opcao" type="checkbox" v-model="hobbie" v-on:keypress="ok = false" value="Leitura" /> Leitura
+      <input class="opcao" type="checkbox" v-model="hobbie" v-on:keypress="ok = false" value="Estudos" /> Estudos
+      <input class="opcao" type="checkbox" v-model="hobbie" v-on:keypress="ok = false" value="Viajar" /> Viajar
+      <hr>
+      <label for="">Linguagem de programação:</label>
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="JavaScript" /> JavaScript
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="Python" /> Python
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="C#" /> C#
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="Php" /> Php
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="SQL" /> SQL
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="R" /> R
+      <input class="opcao" type="radio" v-on:keypress="ok = false" v-model="linguagem" value="VBA" /> VBA
+      <hr>
+      <label for="">Biografia:</label>
+      <input type="text" v-on:keypress="ok = false" v-model="biografia" style="padding: 30px;"
+        placeholder="Digite sua biografia" />
+      <hr>
+      <label for="">Senha:</label>
+      <input type="password" v-on:keypress="ok = false" v-model="senha" minlength="6" placeholder="Digite sua senha" />
+      <hr>
+      <label for="">Confirmar senha:</label>
+      <input type="password" v-on:keypress="ok = false" v-model="confirma" minlength="6"
+        placeholder="Digite sua senha novamente" />
+      <hr>
+      <button type="submit">Mostrar</button>
     </div>
   </form>
+  <div v-if="ok" class="centro">
+    <p>O nome digitado é: {{ nome }}</p>
+    <p>A data digitada é: {{ data }}</p>
+    <p>O email digitado é: {{ email }}</p>
+    <p>O endereço digitado é: {{ endereco }}</p>
+    <p>A cidade digitada é: {{ cidade }}</p>
+    <p>O estado digitado é: {{ estado }}</p>
+    <p>O hobbie digitado é: {{ hobbie }}</p>
+    <p>A linguagem digitada é: {{ linguagem }}</p>
+    <p>A biografia digitada é: {{ biografia }}</p>
+    <p>A senha digitada é: {{ senha }}</p>
+    <p>A confirmação digitada é: {{ confirma }}</p>
+    <img :src="user.avatar" />
+  </div>
 </template>
 
 <style scoped>
-
 button {
   font-weight: bold;
   color: brown;
 }
+
 .centro {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin: 0 auto;
-      margin-top: 3%;
-      background-color: brown;
-      border-radius: 10px;
-      width: 50%;
-      color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 3%;
+  background-color: brown;
+  border-radius: 10px;
+  width: 50%;
+  color: aliceblue;
 }
-
-
 
 
 </style>
